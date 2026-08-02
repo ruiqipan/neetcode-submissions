4c24600f7d597de5591c800aaa028621a776c2f5
Ruiqi Pan <ruiqipan@seas.upenn.edu>
2026-08-01 21:03:53 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hm = defaultdict(int) # key = num, val = index
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hm.keys():
+                return [hm[complement], i]
+            hm[num] = i
+        return [-1, -1]
