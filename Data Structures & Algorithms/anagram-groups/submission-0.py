cc43ea36490358e5f8ead6b30fdbaf59a85a61c9
Ruiqi Pan <ruiqipan@seas.upenn.edu>
2026-08-01 21:38:20 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hm = defaultdict(list)
+        for s in strs:
+            pattern = [0] * 26
+            for c in s:
+                pattern[ord(c) - ord('a')] += 1
+            pattern = tuple(pattern)
+            hm[pattern].append(s)
+        return list(hm.values())
