5680079248e439594073ceba02dd3e38372d7d91
Ruiqi Pan <ruiqipan@seas.upenn.edu>
2026-08-01 21:05:45 -0400

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hs = set()
+        for num in nums:
+            if num in hs:
+                return True
+            hs.add(num)
+        return False
