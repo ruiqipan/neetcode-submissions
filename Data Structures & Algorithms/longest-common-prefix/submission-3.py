4b8e0fb45bebd24db881e438f59772cc0cb9a749
Ruiqi Pan <ruiqipan@seas.upenn.edu>
2026-08-11 23:57:09 -0400

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        currChar = set()
+        currIdx = 0
+        ans = ""
+        while True:
+            for i in range(len(strs)):
+                if currIdx < len(strs[i]):
+                    currChar.add(strs[i][currIdx]) 
+                else:
+                    return ans
+            if len(currChar) > 1:
+                return ans
+            ans += currChar.pop()
+            currIdx += 1
+        return ans
